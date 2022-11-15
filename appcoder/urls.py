0eb53ecc0440b063a745afe03c34e4c17ced9567
Martin Lopes <martin.lopes11@gmail.com>
2022-11-15 18:33:21 -0300

    Formularios

--- appcoder/urls.py
+++ appcoder/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", inicio),
     path("estudiantes/", estudiantes),
     path("profesores/", profesores),
+    path("profesores/crear", creacion_profesores),
     path("cursos/re-locos", cursos, name="coder-cursos"),
-    path("entregables/", entregables, name="coder-entregables")
+    path("entregables/", entregables, name="coder-entregables"),
+    path("curso/crear/", creacion_curso)
 ]
--- appcoder/views.py
+++ appcoder/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from appcoder.forms import *
 from appcoder.models import *
+from django.shortcuts import render
+
 
 # Create your views here.
 
@@ -16,5 +18,38 @@ def estudiantes(request):
 def profesores(request):
     return render(request, "appcoder/profesores.html")
 
+def creacion_profesores(request):
+    
+    if request.method == "POST":
+        formulario = ProfesorFormulario(request.POST)
+        
+        # validamos que el formulario no tenga problemas
+        if formulario.is_valid():
+            #recuperamos los datos del atributo cleaned_data
+            data = formulario.cleaned_data
+
+            profesor = Profesor(nombre=data["nombre"], apellido=data["apellido"], email=data["email"], profesion=data["profesion"])
+
+            profesor.save()
+            return render(request, "appcoder/index.html")
+        
+
+    else:
+        formulario = ProfesorFormulario()
+
+    contexto = {"formulario": formulario}
+    return render(request, "appcoder/profesores_formularios.html", contexto)
+
 def entregables(request):
     return render(request, "appcoder/entregables.html")
+
+def creacion_curso(request):
+
+    if request.method == "POST":
+        nombre_curso = request.POST["curso"]
+        numero_camada = request.POST["camada"]
+
+        curso = Curso(nombre=nombre_curso, camada=numero_camada)
+        curso.save()
+
+    return render(request, "appcoder/curso_formulario.html")
